38e9727cc62389c5b0c7bde7b6f32fa2ae2dae2a
Vít Kilián <kilianv@spseplzen.cz>
2021-08-24 11:22:38 +0200

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-
+import Header from './components/Header';
+import Homepage from './pages/Homepage';
+import './default.scss';
 
 function App() {
   return (
     <div className="App">
+        <Header />
+      <div className="main">
+        <Homepage />
+      </div>
     </div>
   );
 }
