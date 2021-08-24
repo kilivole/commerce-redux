1c21310e8f951810584d926d35bc2622ba5fff91
Vít Kilián <kilianv@spseplzen.cz>
2021-08-24 12:26:37 +0200

    react router / custom layout / footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import './styles.scss';
+
+const Footer = props => {
+    return(
+        <footer className="footer">
+            <div className="wrap">
+                @ KiliVOLE VOE 2021
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
